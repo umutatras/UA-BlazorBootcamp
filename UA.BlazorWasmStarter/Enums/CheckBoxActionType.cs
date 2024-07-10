d04a630c2f88a065a1791cb106adbc7c4bf53f17
umutatras <umutatras@gmail.com>
2024-07-10 22:35:51 +0300

    şifre tasarım ve geliştirmeleri

---
+++ UA.BlazorWasmStarter/Enums/CheckBoxActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace UA.BlazorWasmStarter.Enums
+{
+    public enum CheckBoxActionType
+    {
+        Numbers=1,
+        Lowercase=2,
+        Uppercase=3,
+        SpecialChars=4
+    }
+}
