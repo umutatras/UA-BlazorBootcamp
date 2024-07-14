f2766f2829f90832815528afaf837f758e1f7b71
umutatras <umutatras@gmail.com>
2024-07-14 13:05:17 +0300

    gpt configuration

--- UA.BlazorOpenAIStarter/Program.cs
+++ UA.BlazorOpenAIStarter/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using OpenAI.Extensions;
+using OpenAI.Interfaces;
+using System;
 using UA.BlazorOpenAIStarter;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-var openAIApikey = builder.Configuration.GetSection("OpenAIApiKey").Value;
-builder.Services.AddOpenAIService(settings => { settings.ApiKey = Environment.GetEnvironmentVariable(openAIApikey); });
+
+var openAIApikey = builder.Configuration["OpenAIApiKey"];
+builder.Services.AddOpenAIService(settings => { settings.ApiKey =openAIApikey; });
 await builder.Build().RunAsync();
