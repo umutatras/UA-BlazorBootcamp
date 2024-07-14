825a9a63a7c21e8435395a283279ec1b30350bda
umutatras <umutatras@gmail.com>
2024-07-14 12:12:01 +0300

    openai paketi yüklendi

--- UA.BlazorOpenAIStarter/Program.cs
+++ UA.BlazorOpenAIStarter/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.Extensions;
 using UA.BlazorOpenAIStarter;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+var openAIApikey = builder.Configuration.GetSection("OpenAIApiKey").Value;
+builder.Services.AddOpenAIService(settings => { settings.ApiKey = Environment.GetEnvironmentVariable(openAIApikey); });
 await builder.Build().RunAsync();
