5e658a122806e16dd29fd29f0c9dff7ca6b63b17
umutatras <umutatras@gmail.com>
2024-07-20 11:49:33 +0300

    programcs config

--- UA.BlazorGiftSuggestionApp/Program.cs
+++ UA.BlazorGiftSuggestionApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OpenAI.Extensions;
 using UA.BlazorGiftSuggestionApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+var openAIApikey = builder.Configuration["OpenAIApiKey"];
+builder.Services.AddOpenAIService(settings => { settings.ApiKey = openAIApikey; });
 await builder.Build().RunAsync();
