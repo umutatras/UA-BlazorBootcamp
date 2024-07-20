00a165cf10f2619947d21e558eb3432417839d3a
umutatras <umutatras@gmail.com>
2024-07-20 12:22:21 +0300

    RelationshipType enum eklendi

---
+++ UA.BlazorGiftSuggestionApp/Enums/RelationshipType.cs
@@ -0,0 +1,15 @@
+﻿namespace UA.BlazorGiftSuggestionApp.Enums
+{
+    public enum RelationshipType
+    {
+        Friend = 1,
+        Relative = 2,
+        Parent = 3,
+        Brother = 4,
+        Sister = 5,
+        Child = 6,
+        Spouse = 7,
+        Colleague = 8,
+        Other = 9
+    }
+}
