3045981ff203b671e53feb501752be7fcdc4d865
umutatras <umutatras@gmail.com>
2024-07-24 21:28:00 +0300

    impelementasyonlar

--- UA.VideoTransciberApp.ConsoleClient/Program.cs
+++ UA.VideoTransciberApp.ConsoleClient/Program.cs
@@ -1,5 +1,10 @@
-﻿using System.Resources;
+﻿using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using System;
+using System.Resources;
 using Xabe.FFmpeg;
+using OpenAI.Managers;
+using OpenAI;
 
 var videoFilePath = "C:\\Users\\eftel\\Desktop\\Sam-Bankman-Fried.mp4";
 
@@ -13,3 +18,29 @@ await conversion.Start(cts.Token);
 
 Console.WriteLine("Conversion Completed.");
 Console.WriteLine(audioFilePath);
+
+const string fileName = "Sam-Bankman-Fried.mp3";
+var sampleFile = await File.ReadAllBytesAsync(audioFilePath);
+var openAiService = new OpenAIService(new OpenAiOptions()
+{
+    ApiKey = ""
+});
+var audioResult = await openAiService.Audio.CreateTranscription(new AudioCreateTranscriptionRequest
+{
+    FileName = fileName,
+    File = sampleFile,
+    Model = Models.WhisperV1,
+    ResponseFormat = StaticValues.AudioStatics.ResponseFormat.Srt
+});
+if (audioResult.Successful)
+{
+    Console.WriteLine(string.Join("\n", audioResult.Text));
+}
+else
+{
+    if (audioResult.Error == null)
+    {
+        throw new Exception("Unknown Error");
+    }
+    Console.WriteLine($"{audioResult.Error.Code}: {audioResult.Error.Message}");
+}
