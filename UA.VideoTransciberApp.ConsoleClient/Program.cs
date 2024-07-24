40d05e6a6461d0de62aa1bb4809833ab179c478e
umutatras <umutatras@gmail.com>
2024-07-24 20:49:22 +0300

    .

---
+++ UA.VideoTransciberApp.ConsoleClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using Xabe.FFmpeg;
+
+var videoFilePath = "C:\\Users\\eftel\\Desktop\\Sam-Bankman-Fried.mp4";
+
+string audioFilePath = Path.ChangeExtension(videoFilePath, "mp3");
+//FFmpeg.SetExecutablesPath(); dosya yolunuda vererek proje içinde tutmadan işlem yapabiliriz.
+
+CancellationTokenSource cts=new CancellationTokenSource();
+var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(videoFilePath, audioFilePath);
+await conversion.Start(cts.Token);
+
+
+Console.WriteLine("Conversion Completed.");
+Console.WriteLine(audioFilePath);
