8d76c1671b2f17a76b30f6c833b72eab87908e28
umutatras <umutatras@gmail.com>
2024-07-27 12:45:56 +0300

    bug düzeltme

--- VideoTransciberApp.BlazorUI/VideoTransciberApp.BlazorUI.Client/Program.cs
+++ VideoTransciberApp.BlazorUI/VideoTransciberApp.BlazorUI.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5214/api/") });
+var app = builder.Build();
 await builder.Build().RunAsync();
