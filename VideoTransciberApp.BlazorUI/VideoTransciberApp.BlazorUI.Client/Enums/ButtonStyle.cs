6d91c491f35e108d8abe578c5b68e0365d7eb333
umutatras <umutatras@gmail.com>
2024-07-28 10:42:07 +0300

    button component

---
+++ VideoTransciberApp.BlazorUI/VideoTransciberApp.BlazorUI.Client/Enums/ButtonStyle.cs
@@ -0,0 +1,17 @@
+﻿namespace VideoTransciberApp.BlazorUI.Client.Enums
+{
+    public enum ButtonStyle
+    {
+        Primary=1,
+        Secondary=2,
+        Success=3,
+        Error=4,
+        Warning=5,
+        Info=6,
+        Neutral=7,
+        Accent=8,
+        Link=9,
+        Ghost=10,
+        Outline=11
+    }
+}
